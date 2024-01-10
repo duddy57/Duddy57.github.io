0182fe387f92c48adc57a629d51a27bd439eedf9
Duddy57 <tandinhoduddy9@gmail.com>
2024-01-10 18:16:09 -0300

    Initial commit

---
+++ src/components/Footer.svelte
@@ -0,0 +1,32 @@
+<script>
+    const year = new Date().getFullYear();
+</script>
+
+<footer
+    id="Footer"
+    data-aos="fade-up"
+    class=" py-20 mt-10 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center z-20"
+>
+    <p class="px-4 py-22 text-violet-700 font-medium poppins">Se conecte cmg</p>
+    <div class="flex flex-col gap-4 items-center justify-center">
+        <div
+            class="flex flex-row md:justify-center md:mt-10 lg:justify-center gap-6"
+        >
+            <p>
+                <b class=" text-violet-400pr-2 poppins text-violet-400"
+                    >Email:
+                </b>tandinhoduddy9@gmail.com
+            </p>
+            <a href="https://www.linkedin.com/in/duddy57dev/" class="transition ease-in-out hover:-translate-y-3 hover:scale-110 "
+                ><i class=" fa-brands fa-linkedin fa-xl text-violet-400 hover:text-violet-600"></i></a
+            > 
+            <a href="https://www.instagram.com/duddy57dev_/" class="transition ease-in-out hover:-translate-y-3 hover:scale-110 "
+                ><i class="fa-brands fa-github fa-xl text-violet-400 hover:text-violet-600"></i></a
+            >
+            <a href="https://www.instagram.com/duddy57dev_/" class=" text-violet-400 transition ease-in-out hover:-translate-y-3 hover:scale-110 hover:text-violet-600"
+                ><i class="fa-brands fa-instagram fa-xl"></i></a
+            >
+        </div>
+        <p>Trafyy&copy; {year}</p>
+    </div>
+</footer>
---
+++ src/components/Header.svelte
@@ -0,0 +1,34 @@
+<script>
+    export let y;
+
+    let tabs = [
+        { name: "#about", label: "Sobre mim" },
+        { name: "#projects", label: "Projetos" },
+        { name: "#skils", label: "Habilidades" },
+    ];
+</script>
+
+<header
+    class={"sticky z-50 top-0 duration-200 px-8 flex items-center justify-between border border-solid backdrop-blur-sm bg-transparent" +
+        (y > 0
+            ? " py-4 bg-slate-950 border-violet-950"
+            : " py-6 bg-transparent border-transparent")}
+>
+    <a href="/"><img src="Logo.svg" alt="Logo" class=" h-10 w-10" /></a>
+    <div class="sm:flex items-center gap-6 hidden font-bold">
+        {#each tabs as tab, index}
+            <a href={tab.name} class=" duration-200 hover:text-violet-400">
+                <p>{tab.label}</p>
+            </a>
+        {/each}
+        <a
+            href="#Footer"
+            class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950"
+        >
+            <div
+                class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
+            ></div>
+            <h4 class="relative z-9">Contato</h4>
+        </a>
+    </div>
+</header>
